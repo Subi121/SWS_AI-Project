c999e1f9047c585c53bb3b8d46fc60dbed1a6d73
subiksha <subikshaseventhirajan@gmail.com>
2026-05-29 16:17:27 +0530

    update and enhance backend functionality

--- sws-backend/backend/src/main/java/com/sws/backend/service/FileService.java
+++ sws-backend/backend/src/main/java/com/sws/backend/service/FileService.java
@@ -1,37 +1,4 @@
 
-// package com.sws.backend.service;
-
-// import com.sws.backend.entity.FileEntity;
-// import com.sws.backend.repository.FileRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import org.springframework.web.multipart.MultipartFile;
-
-// import java.io.IOException;
-// import java.util.List;
-
-// @Service
-// public class FileService {
-
-//     @Autowired
-//     private FileRepository fileRepository;
-
-//     public FileEntity uploadFile(MultipartFile file) throws IOException {
-
-//         FileEntity fileEntity = new FileEntity();
-
-//         fileEntity.setFileName(file.getOriginalFilename());
-//         fileEntity.setStatus("UPLOADED");
-//         fileEntity.setData(file.getBytes());
-
-//         return fileRepository.save(fileEntity);
-//     }
-
-//     public List<FileEntity> getAllFiles() {
-
-//         return fileRepository.findAll();
-//     }
-// }
 package com.sws.backend.service;
 
 import com.sws.backend.entity.FileEntity;
