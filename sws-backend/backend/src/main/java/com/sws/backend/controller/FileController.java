a665462a70462b29931a425a85d442be4435edae
subiksha <subikshaseventhirajan@gmail.com>
2026-05-29 16:14:28 +0530

    Backend controller updated

--- sws-backend/backend/src/main/java/com/sws/backend/controller/FileController.java
+++ sws-backend/backend/src/main/java/com/sws/backend/controller/FileController.java
@@ -1,42 +1,4 @@
 
-// package com.sws.backend.controller;
-
-// import com.sws.backend.entity.FileEntity;
-// import com.sws.backend.service.FileService;
-// import lombok.RequiredArgsConstructor;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-// import org.springframework.web.multipart.MultipartFile;
-
-// import java.io.IOException;
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/api/files")
-// @RequiredArgsConstructor
-// @CrossOrigin(origins = "http://localhost:5173")
-// public class FileController {
-
-//     private final FileService fileService;
-
-//     @PostMapping("/upload")
-//     public ResponseEntity<FileEntity> uploadFile(
-//             @RequestParam("file") MultipartFile file
-//     ) throws IOException {
-
-//         return ResponseEntity.ok(
-//                 fileService.uploadFile(file)
-//         );
-//     }
-
-//     @GetMapping
-//     public ResponseEntity<List<FileEntity>> getAllFiles() {
-
-//         return ResponseEntity.ok(
-//                 fileService.getAllFiles()
-//         );
-//     }
-// }
 package com.sws.backend.controller;
 
 import com.sws.backend.entity.FileEntity;
